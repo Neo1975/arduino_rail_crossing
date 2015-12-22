4a0eb57bd5f258e11f68eb1bc223361f6a247684
Neo1975 <d.fornaciari@gmail.com>
2015-12-22 17:53:10 +0100

    Review code for a better implementation instead use delay function

--- passaggio_livello/step4/step4.ino
+++ passaggio_livello/step4/step4.ino
@@ -1,80 +1,76 @@
 #include <Servo.h>
 
+#include <button.h>
 #include <led.h>
-//#define DEGUB 1
+//#define DEBUG 1
 
 #define BLINK_TIME 500
+#define PIN_LEFT_LED 10
+#define PIN_RIGHT_LED 6
+#define PIN_LEFT_BUTTON 12
+#define PIN_RIGHT_BUTTON 13
+#define PIN_SERVO 5
 
-bool pushingButton = false;
+#define SERVO_START_ANGLE 0
+#define SERVO_END_ANGLE 82
+
 bool blinking = false;
-bool servoMoved = false;
 
-Led ledLeftRed(10);
-Led ledRightRed(6);
+Led ledLeftRed;
+Led ledRightRed;
+Button btnLeft;
+Button btnRight;
+byte btnSrcAction;
 Servo myServo;
 
+long pressTime = 0;
+
+void onPressed(Button src, Button::EventType eventType) {
+#ifdef DEBUG
+    Serial.println("onPressed");
+#endif
+
+  if (btnSrcAction==0 || btnSrcAction != src.getPin()){
+    if(eventType == Button::RELEASED){
+      blinking = !blinking;
+      myServo.write(blinking ? SERVO_END_ANGLE : SERVO_START_ANGLE);
+      pressTime = millis();
+      btnSrcAction = (btnSrcAction==0 ? src.getPin() : 0);
+    }
+  }
+}
+
 void setup(){
-  ledLeftRed.init();
-  ledRightRed.init();
-  pinMode(13,INPUT);
-  pinMode(12,INPUT);
-  myServo.attach(5);
-#ifdef DEGUB
+  btnLeft.init(PIN_LEFT_BUTTON, onPressed);
+  btnRight.init(PIN_RIGHT_BUTTON, onPressed);
+
+  ledLeftRed.init(PIN_LEFT_LED);
+  ledRightRed.init(PIN_RIGHT_LED);
+
+  myServo.attach(PIN_SERVO);
+  myServo.write(SERVO_START_ANGLE);
+  
+  btnSrcAction = 0;
+
+#ifdef DEBUG
   Serial.begin(9600);
 #endif
-  myServo.write(0);
 }
 
 void loop(){
-  bool v = false;
-  if (!blinking){
-#ifdef DEGUB
-        Serial.println("Not Blinking ");
-#endif
-
-    v = digitalRead(13);
-    delay(1);
-
-    if (v && v != pushingButton){
-#ifdef DEGUB
-      Serial.println("Left Button Pushed ");
-#endif
-      blinking = true;
-      if (!servoMoved){
-        myServo.write(82);
-        servoMoved = true;
-      }
-    }
-  } else{
-#ifdef DEGUB
-        Serial.println("Blinking ");
-#endif
-
-      v = digitalRead(12);
-      delay(1);
-
-      if (v && v != pushingButton){
-#ifdef DEGUB
-        Serial.println("Right Button Pushed ");
-#endif
-        blinking = false;
-      if (servoMoved){
-        myServo.write(0);
-        servoMoved = false;
-      }
-      }
-  }
-
+  btnLeft.update();
+  btnRight.update();
+  
+  ledLeftRed.off();
+  ledRightRed.off();
+  
   if (blinking){
-    ledLeftRed.on();
-    delay(BLINK_TIME);
-    ledLeftRed.off();
-    ledRightRed.on();
-    delay(BLINK_TIME);
-    ledRightRed.off();
+    if (((millis()-pressTime)/BLINK_TIME)%2 < 1){
+      ledLeftRed.on();
+    } else{
+      ledRightRed.on();
+    }
   }
-
-  pushingButton = v;
-
 }
+
 
